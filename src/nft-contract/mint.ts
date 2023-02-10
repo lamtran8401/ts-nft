90ec46fcbc28ffd0ad0e67cf72eded7921b27e41
tranvulamdev <tranvulam.dev@gmail.com>
2023-02-10 13:30:50 +0700

    update approval branch - royalty func

--- src/nft-contract/mint.ts
+++ src/nft-contract/mint.ts
@@ -1,48 +1,68 @@
 // @ts-nocheck
-import { assert, near } from "near-sdk-js";
-import { Contract, NFT_METADATA_SPEC, NFT_STANDARD_NAME } from ".";
-import { internalAddTokenToOwner, refundDeposit } from "./internal";
-import { Token, TokenMetadata } from "./metadata";
+import { assert, near } from 'near-sdk-js'
+import { Contract, NFT_METADATA_SPEC, NFT_STANDARD_NAME } from '.'
+import { internalAddTokenToOwner, refundDeposit } from './internal'
+import { Token, TokenMetadata } from './metadata'
 
 export function internalMint({
-    contract,
-    tokenId,
-    metadata,
-    receiverId,
-    perpetualRoyalties
-}:{ 
-    contract: Contract, 
-    tokenId: string, 
-    metadata: TokenMetadata, 
-    receiverId: string 
-    perpetualRoyalties: {[key: string]: number}
+	contract,
+	tokenId,
+	metadata,
+	receiverId,
+	perpetualRoyalties,
+}: {
+	contract: Contract
+	tokenId: string
+	metadata: TokenMetadata
+	receiverId: string
+	perpetualRoyalties: { [key: string]: number }
 }): void {
-    //measure the initial storage being used on the contract TODO
-    let initialStorageUsage = near.storageUsage();
+	//measure the initial storage being used on the contract TODO
+	let initialStorageUsage = near.storageUsage()
 
-    //specify the token struct that contains the owner ID 
-    let token = new Token ({
-        //set the owner ID equal to the receiver ID passed into the function
-        ownerId: receiverId,
-        //we set the approved account IDs to the default value (an empty map)
-        approvedAccountIds: {},
-        //the next approval ID is set to 0
-        nextApprovalId: 0
-    });
+	// create a royalty map to store in the token
+	let royalty: { [accountId: string]: number } = {}
 
-    //insert the token ID and token struct and make sure that the token doesn't exist
-    assert(!contract.tokensById.containsKey(tokenId), "Token already exists");
-    contract.tokensById.set(tokenId, token)
+	// if perpetual royalties were passed into the function: TODO: add isUndefined fn
+	if (perpetualRoyalties != null) {
+		//make sure that the length of the perpetual royalties is below 7 since we won't have enough GAS to pay out that many people
+		assert(
+			Object.keys(perpetualRoyalties).length < 7,
+			'Cannot add more than 6 perpetual royalty amounts'
+		)
 
-    //insert the token ID and metadata
-    contract.tokenMetadataById.set(tokenId, metadata);
+		//iterate through the perpetual royalties and insert the account and amount in the royalty map
+		Object.entries(perpetualRoyalties).forEach(([account, amount], index) => {
+			royalty[account] = amount
+		})
+	}
 
-    //call the internal method for adding the token to the owner
-    internalAddTokenToOwner(contract, token.owner_id, tokenId)
+	//specify the token struct that contains the owner ID
+	let token = new Token({
+		//set the owner ID equal to the receiver ID passed into the function
+		ownerId: receiverId,
+		//we set the approved account IDs to the default value (an empty map)
+		approvedAccountIds: {},
+		//the next approval ID is set to 0
+		nextApprovalId: 0,
+		//the map of perpetual royalties for the token (The owner will get 100% - total perpetual royalties)
+		royalty,
+	})
 
-    //calculate the required storage which was the used - initial TODO
-    let requiredStorageInBytes = near.storageUsage().valueOf() - initialStorageUsage.valueOf();
+	//insert the token ID and token struct and make sure that the token doesn't exist
+	assert(!contract.tokensById.containsKey(tokenId), 'Token already exists')
+	contract.tokensById.set(tokenId, token)
 
-    //refund any excess storage if the user attached too much. Panic if they didn't attach enough to cover the required.
-    refundDeposit(requiredStorageInBytes);
+	//insert the token ID and metadata
+	contract.tokenMetadataById.set(tokenId, metadata)
+
+	//call the internal method for adding the token to the owner
+	internalAddTokenToOwner(contract, token.owner_id, tokenId)
+
+	//calculate the required storage which was the used - initial TODO
+	let requiredStorageInBytes =
+		near.storageUsage().valueOf() - initialStorageUsage.valueOf()
+
+	//refund any excess storage if the user attached too much. Panic if they didn't attach enough to cover the required.
+	refundDeposit(requiredStorageInBytes)
 }
